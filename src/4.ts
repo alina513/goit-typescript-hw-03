b7036d1e4c44ba15fbc3e453abc2155d95b5107d
alina513 <130853290+alina513@users.noreply.github.com>
2023-12-22 09:41:05 +0200

    4 all

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,45 @@
+class Key {
+    constructor(private readonly signature: number = Math.random()) {}
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    constructor(private readonly key: Key) {}
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected tenants: Person[] = [];
+    constructor(protected door: boolean, protected key: Key) {}
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+      }
+      return;
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    constructor(key: Key, door: boolean = false) {
+      super(door, key);
+    }
+  
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+      }
+      return;
+    }
+  }
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +48,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
